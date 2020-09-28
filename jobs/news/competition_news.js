b5270223b3c25ce3fb26b8684e4761fe20ebef1a
amenline <amenline.07@gmail.com>
2020-09-28 00:52:14 +0000

    saved the fetched news to redis

--- jobs/news/competition_news.js
+++ jobs/news/competition_news.js
@@ -1,10 +1,10 @@
 const fetch = require('node-fetch');
 
-// const base url
+// const espn hidden api base url
 const main_url = 'http://site.api.espn.com/apis/site/v2/sports/soccer/';
 
-// Fetch Premier League news
-const fetch_league = async (league) => {
+// Fetch league news
+const fetch_news = async (league) => {
   const res = await fetch(`${main_url}${league}/news`);
   const response = await res.json();
   const links = response.articles.map((article) => article.links.api.news.href);
@@ -16,14 +16,8 @@ const fetch_league = async (league) => {
     return fetch_res;
   });
   const news = await Promise.all(news_promise);
-  console.log(news);
+  // console.log(news);
   return news;
 };
 
-const premier_league_news = fetch_league('eng.1');
-const champions_league = fetch_league('uefa.champions');
-const la_liga_news = fetch_league('esp.1');
-const europa_league_news = fetch_league('uefa.europa');
-const serie_a_news = fetch_league('ita.1');
-const major_league_soccer_news = fetch_league('USA.1');
-const bundesliga_news = fetch_league('ger.1');
+module.exports = fetch_news;
--- jobs/news/index.js
+++ jobs/news/index.js
@@ -1,13 +1,43 @@
-var CronJob = require('cron').CronJob;
-var fetch = require('node-fetch');
+const redis = require('redis');
+const fetch_competition_news = require('./competition_news');
+const { promisify } = require('util');
 
-// const
+let client = redis.createClient();
+client.on('error', function (error) {
+  console.error(error);
+});
+const setAsync = promisify(client.set).bind(client);
 
-var home_news = new CronJob(
-  '1 * * * * *',
-  function () {
-    console.log('Will update the home news section frequently');
-  },
-  null,
-  true
-);
+const fetch_and_save = async (redis_key, espn_name) => {
+  const news = await fetch_competition_news(espn_name);
+  const response = await setAsync(redis_key, JSON.stringify(news));
+  console.log(`${redis_key} :`, response);
+};
+
+const competition_news = () => {
+  // main competitions
+  // eng.1
+  fetch_and_save('premier_league_news', 'eng.1');
+  // uefa.champions
+  fetch_and_save('champions_league_news', 'uefa.champions');
+  // esp.1
+  fetch_and_save('la_liga_news', 'esp.1');
+  // uefa.europa
+  fetch_and_save('europa_league_news', 'uefa.europa');
+  // ita.1
+  fetch_and_save('serie_a_news', 'ita.1');
+  // usa.1
+  fetch_and_save('major_league_soccer_news', 'usa.1');
+  // ger.1
+  fetch_and_save('bundesliga_news', 'ger.1');
+  // ger.2
+  fetch_and_save('bundesliga2_news', 'ger.2');
+
+  // other competitions
+  // uefa.euroq
+  fetch_and_save('eu_championship_qualifying_news', 'uefa.euroq');
+  // concacaf.nations.league
+  fetch_and_save('concacaf_nations_news', 'concacaf.nations.league');
+};
+
+competition_news();
